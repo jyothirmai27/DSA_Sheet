8c633d6e2b928e779fc3dd38e0a92563e1eefbdb
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-01-30 10:16:53 +0530

    Update Brute.cpp

--- Arrays/Dutch National Flag/Brute.cpp
+++ Arrays/Dutch National Flag/Brute.cpp
@@ -1,2 +1,13 @@
-LeetCode - https://leetcode.com/problems/sort-colors/
+// LeetCode - https://leetcode.com/problems/sort-colors/
+// Time Complexity - O(nlogn)
+// Space Complexity - O(1)
+// Sorting technique
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n= nums.size();
+         sort(nums.begin(),nums.end());
+    }
+}
 
