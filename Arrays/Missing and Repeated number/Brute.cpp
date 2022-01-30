c8278bf9a541330cc038aac939a543da5c7e8f74
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-01-30 12:06:32 +0530

    Update Brute.cpp

--- Arrays/Missing and Repeated number/Brute.cpp
+++ Arrays/Missing and Repeated number/Brute.cpp
@@ -1,1 +1,29 @@
+// GeeksforGeeks - https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1/#
+// Time Complexity - O(N) + O(N) = O(N)
+// Space Complexity - O(N)
+//Hashing Technique
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int temp[n+1]={0};
+    for(int i=0;i<n;i++)
+    {
+        temp[arr[i]]++;
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        if(temp[i]==0)
+            cout<<"Missing Number = "<<i<<endl;
+        else if(temp[i]==2)
+            cout<<"Repeating Number = "<<i<<endl;
+    }
+    return 0;
+}
