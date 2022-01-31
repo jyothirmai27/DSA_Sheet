79d0a95e6dfd974bba5b9512f156d406778e6894
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-01-31 22:53:54 +0530

    Update Brute.cpp

--- Arrays/Kadanes Algorithm/Brute.cpp
+++ Arrays/Kadanes Algorithm/Brute.cpp
@@ -6,15 +6,6 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        /*int curr_sum=0,max_sum=nums[0];
-          for(int i=0;i<nums.size();i++)
-          {
-            curr_sum = curr_sum + nums[i];
-            max_sum = max(max_sum,curr_sum);
-            curr_sum = max(curr_sum,0);
-          }
-           
-        return max_sum;*/
         int n = nums.size();
         int sum = 0 ,max_sum = nums[0];
         for(int i=0;i<n;i++)
