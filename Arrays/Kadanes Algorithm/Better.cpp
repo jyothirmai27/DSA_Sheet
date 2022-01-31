519f3c8f7fdfd8a55309e8b9c729aa17eba889cf
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-01-31 23:52:22 +0530

    Update Better.cpp

--- Arrays/Kadanes Algorithm/Better.cpp
+++ Arrays/Kadanes Algorithm/Better.cpp
@@ -1,1 +1,33 @@
+// Time Complexity - O(N*N) = O(N^2)
+// Space Complexity - O(N) - For calculating prefix sum 
+// Prefix sum is calculated to avoid repetitions 
 
+class Solution {
+public:
+   int n = nums.size();
+        int prefix_sum[n];
+        prefix_sum[0] = nums[0];
+        int sum,max_sum=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            prefix_sum[i] = prefix_sum[i-1] + nums[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            sum = 0;
+            for(int j=i;j<n;j++)
+            {
+               if(i>0)
+               {
+                   sum = prefix_sum[j] - prefix_sum[i-1];
+               }
+               else
+               {
+                    sum = prefix_sum[j];
+               }
+               max_sum = max(max_sum,sum);
+            }
+        }
+        return max_sum;
+    }
+};
