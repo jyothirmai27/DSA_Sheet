95c819f3a92becd20c1951e11cbc629de0a43f04
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-02-01 09:12:44 +0530

    Update Brute.cpp

--- Arrays/Duplicate Number/Brute.cpp
+++ Arrays/Duplicate Number/Brute.cpp
@@ -1,1 +1,22 @@
-
+// LeetCode - https://leetcode.com/problems/find-the-duplicate-number/
+// Time Complexity - O(NlogN) + O(N) = O(NlogN)
+// Space Complexity - O(1)
+// Sorting technique
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int ans;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                ans = nums[i];
+                break;
+            }
+        }
+        return ans;
+    }
+};
