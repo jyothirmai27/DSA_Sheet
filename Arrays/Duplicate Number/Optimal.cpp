46ec63b7201de472b8b3b4dee2a8a97a9583c468
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-02-01 09:16:13 +0530

    Update Optimal.cpp

--- Arrays/Duplicate Number/Optimal.cpp
+++ Arrays/Duplicate Number/Optimal.cpp
@@ -1,1 +1,27 @@
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+// Floyd's Cycle detection algo - slow & fast pointer concept
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      
+        int slow = nums[0];
+        int fast = nums[0];
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        while(fast != slow)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+       
+    }
+};
