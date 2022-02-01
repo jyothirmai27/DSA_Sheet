01c3c8128ca3666a01454eebc9f5ef94ec0f649c
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-02-01 09:14:01 +0530

    Update Better.cpp

--- Arrays/Duplicate Number/Better.cpp
+++ Arrays/Duplicate Number/Better.cpp
@@ -1,1 +1,26 @@
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+// Hashing technique
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int n=nums.size();
+        int res;
+        vector<int> temp(n+1,0);
+        for(int i=0;i<n;i++)
+        {
+            temp[nums[i]]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(temp[i] >=2)
+            {
+                res = i;
+            }
+        }
+        return res;
+        
+    }
+};
