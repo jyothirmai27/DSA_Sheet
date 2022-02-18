2440075aebe1bbcdf25f24dacba4c072ad88e265
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-02-19 01:12:56 +0530

    Create Iterative.cpp

---
+++ Binary Search Trees/Inorder/Iterative.cpp
@@ -0,0 +1,30 @@
+LeetCode - https://leetcode.com/problems/binary-tree-inorder-traversal/
+ TC - O(N)
+ SC - O(N) (Auxiliary Space)
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> s;
+         TreeNode* node = root;
+        while(true)
+        {
+           if(node!=NULL)
+           {
+               s.push(node);;
+               node = node->left;
+           }
+            else
+            {
+                if(s.empty())
+                    break;
+                node = s.top();
+                s.pop();
+                ans.push_back(node->val);
+                node = node->right;        
+            }
+        }
+        return ans;
+        
+    }
+};
